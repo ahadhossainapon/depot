66b29d14f41a0b0e8aed101f03e1a0bb5fb29c43
ahadhossainapon <ahadhossain73@gmail.com>
2018-03-22 19:19:58 +0600

    add login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,9 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  before_filter :authorize
   protect_from_forgery with: :exception
+  before_action :load_cart
 
   private
 
@@ -13,5 +15,15 @@ class ApplicationController < ActionController::Base
     cart
   end
 
+  def load_cart
+    @cart = current_cart
+  end
+  protected
+
+  def authorize
+    unless User.find_by_id(session[:user_id])
+      redirect_to login_url, :notice => "Please log in"
+    end
+  end
 
 end
